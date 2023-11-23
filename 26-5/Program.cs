560521e7d1db43ca0ab03996d758e77aa749aced
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-11-24 00:07:06 +0300

    Add files via upload

---
+++ 26-5/Program.cs
@@ -0,0 +1,11 @@
+﻿double n=Convert.ToDouble(Console.ReadLine());
+double m  = Convert.ToDouble(Console.ReadLine());
+double k = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+
+for (; n<m; n = n+1)
+{
+    k=Math.Pow(b+k,2);
+}
+
+Console.WriteLine(k);
